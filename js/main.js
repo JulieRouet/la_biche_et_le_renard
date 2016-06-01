f159d236f88e228039c16e79280497f54510bae8
julie rouet <jrouet.work@gmail.com>
2016-06-01 17:55:26 +0200

    add blog and concept pages

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// animation ballon
+
+
+
+
+//
+//                                 // ballon three
+$(document).ready(function(){
+
+  $("#animatethree").click(function() {
+
+    $("#three").animate(
+
+      {
+
+        top: "-900"
+
+
+      }, 2000);
+
+  });//animate
+
+});//Doc ready
+
+
+//                        // ballon four
